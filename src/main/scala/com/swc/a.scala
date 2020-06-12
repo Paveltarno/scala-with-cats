7ccd6b2ecdf181470591e4dc5b7cd327c85c14d2
Pavel Tarnopolsky <pavel.tarnopolsky@soundcloud.com>
2020-06-12 13:37:34 +0200

    update

--- src/main/scala/com/swc/a.scala
+++ src/main/scala/com/swc/a.scala
@@ -6,8 +6,5 @@ import cats.instances.int._
 
 object Foo {
 
-  val intToString: Int => String = _.toString()
 
-  val qq = Functor[Option].map(Some(1))(intToString) // Result is Option[String]
-  val qe = Functor[Option].lift(intToString)(Some(1)) // Result is Option[String]
 }
--- src/test/scala/example/3-6.scala
+++ src/test/scala/example/3-6.scala
@@ -4,6 +4,8 @@ import org.scalatest.funspec.AnyFunSpec
 import com.swc.printable.Printable
 import com.swc.printable.PrintableInstances
 
+final case class Box[A](value: A)
+
 class ShowingOffContramapSpec extends AnyFunSpec {
 
   it("Uses contramap to convert from Printable[Int] to Printable[Double]") {
@@ -13,8 +15,6 @@ class ShowingOffContramapSpec extends AnyFunSpec {
   }
 
   describe("With Box class") {
-
-    final case class Box[A](value: A)
 
     implicit val stringPrintable: Printable[String] =
       new Printable[String] {
@@ -39,6 +39,52 @@ class ShowingOffContramapSpec extends AnyFunSpec {
     it("Should format Box[Boolean]") {
       assert(Printable.format(Box(true)) == "yes")
     }
+  }
+
+}
+
+class ImapSpec extends AnyFunSpec {
+
+  trait Codec[A] {
+    self =>
+    def encode(v: A): String
+    def decode(v: String): A
+    def imap[B](dec: A => B, enc: B => A): Codec[B] = {
+      new Codec[B] {
+        override def encode(v: B): String = self.encode(enc(v))
+        override def decode(v: String): B = dec(self.decode(v))
+      }
+    }
+  }
+
+  def encode[A](value: A)(implicit c: Codec[A]): String = c.encode(value)
+  def decode[A](value: String)(implicit c: Codec[A]): A = c.decode(value)
+
+  implicit val codecString = new Codec[String] {
+    override def encode(v: String): String = v
+    override def decode(v: String): String = v
+  }
+
+  implicit val codecDouble =
+    codecString.imap[Double]((x) => x.toDouble, (x) => x.toString())
+  it("Transforms Codec[String] -> Codec[Double]") {
+
+    assert(encode(2.0) == "2.0")
+    assert(decode[Double]("2.0") == 2.0)
+
+  }
+
+  it("Transforms Code[String] to Code[Box[Double]]") {
+
+    implicit def codecBox[A](implicit codec: Codec[A]): Codec[Box[A]] = {
+      codec.imap[Box[A]](
+        Box(_),
+        _.value
+      )
+    }
+
+    assert(encode(Box(123.4)) == "123.4")
+    assert(decode[Box[Double]]("123.4") == Box(123.4))
   }
 
 }
