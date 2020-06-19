f7fafebb4333feaa711d38a3643d48f332386627
Pavel Tarnopolsky <pavel.tarnopolsky@soundcloud.com>
2020-06-19 17:13:11 +0200

    update

--- src/test/scala/example/4-1.scala
+++ src/test/scala/example/4-1.scala
@@ -7,6 +7,8 @@ class GettingFuncySpec extends AnyFunSpec {
   trait Monad[F[_]] {
     def pure[A](in: A): F[A]
     def flatMap[A, B](source: F[A])(fn: A => F[B]): F[B]
+
+    // Implement this
     def map[A, B](source: F[A])(fn: A => B): F[B] =
       flatMap(source)((q) => pure(fn(q)))
 
---
+++ src/test/scala/example/4-3.scala
@@ -0,0 +1,22 @@
+package example
+
+import org.scalatest.funspec._
+import cats.Id
+
+class MonadicSecretIdentities extends AnyFunSpec {
+  def pure[A](in: A): Id[A] = in
+  def map[A, B](in: Id[A])(f: A => B) = f(in)
+  def flatMap[A, B](in: Id[A])(f: A => Id[B]) = f(in)
+
+  it("has a pure method") {
+    assert(pure(1) == 1)
+  }
+
+  it("has a map method"){
+    assert(map(1)(_.toString()) == "1")
+  }
+
+  it("has a flatmap method"){
+    assert(flatMap(1)(_.toString()) == "1")
+  }
+}
